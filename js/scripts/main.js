7b980f34dd890fdb966a3cee662aa90e74a303b0
Vitor-Linaris <vitor.linaris@gmail.com>
2023-02-16 22:27:25 -0300

    feat: add menu esportes and animation

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -63,3 +63,30 @@ document.onkeydown = function(e) {
     tagHtml.classList.remove('show-modal');
   }
 }
+
+const btnMenu = document.querySelectorAll('.js-btn-menu');
+const menuSite =document.querySelectorAll('.js-menu');
+
+btnMenu.forEach((btn, index) => {
+  btn.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    menuSite.forEach(itemMenu => {
+      itemMenu.classList.remove('active');
+      
+      itemMenu.addEventListener('mouseleave', () => {
+        itemMenu.classList.remove('active');
+        btnMenu.forEach(itemBtn => {
+          itemBtn.classList.remove('active');
+        })
+      })
+    })
+
+    btnMenu.forEach(itemBtn => {
+      itemBtn.classList.remove('active');
+    })
+
+    btn.classList.add('active');
+    menuSite[index].classList.add('active');
+  })
+})
